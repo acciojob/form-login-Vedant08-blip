ec76a0a82cb69036ea2951dbed32c6dfa0a74b25
Vedant08-blip <vedanttrivedi87@gmail.com>
2025-06-20 10:34:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 function getFormvalue() {
     //Write your code here
+	function getFormvalue(event) {
+  event.preventDefault(); // Prevent form reload
+
+  const firstName = document.forms["userForm"]["fname"].value.trim();
+  const lastName = document.forms["userForm"]["lname"].value.trim();
+
+  if (!firstName && !lastName) {
+    alert("Please enter your first and last name.");
+    return;
+  }
+
+  alert(`${firstName} ${lastName}`);
+}
 
 }
